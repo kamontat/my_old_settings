1b817f5baab7fc34f208195445f60ac44f07985d
kamontat <kamontat_c@hotmail.com>
2017-07-15 19:12:57 +0700

    Fixed uninstall wrong command

--- uninstall.sh
+++ uninstall.sh
@@ -11,7 +11,7 @@ function remove {
 
 # load cache file, if cache not exist create new one!
 function load_cache {
-    -d $1.cache || -f $1.cache && mv $1.cache $1 || echo "$1 don't have cache file"
+    [ -d $1.cache ] || [ -f $1.cache ] && mv $1.cache $1 || echo "$1 don't have cache file"
 }
 
 remove ~/.shell_prompt.sh
