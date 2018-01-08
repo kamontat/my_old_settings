b05fb960d2c884d40adc5682559ba0c093834dea
Kamontat Chantrachirathumrong <kamontat_c@hotmail.com>
2018-01-08 22:54:21 +0700

    🔖 Dump version: v14

---
+++ .my-zsh/ohmyzsh.zsh
@@ -0,0 +1,4 @@
+export RPS1="%{$reset_color%}"
+export DISABLE_AUTO_UPDATE="true"
+export ENABLE_CORRECTION="true"
+export COMPLETION_WAITING_DOTS="true"
---
+++ .my-zsh/sourcing.zsh
@@ -0,0 +1,3 @@
+test -s "${HOME}/.profile" && source "${HOME}/.profile"
+test -s "${HOME}/.iterm2_shell_integration.zsh" && source "${HOME}/.iterm2_shell_integration.zsh"
+test -s "${HOME}/.fzf.zsh" && source "${HOME}/.fzf.zsh"
