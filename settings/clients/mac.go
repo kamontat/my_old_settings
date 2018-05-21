ec6b3cb03695211e37055a89edcca45b6cd7e712
Kamontat Chantrachirathumrong <kamontat.c@hotmail.com>
2018-05-22 01:58:26 +0700

    [doc] Add Mac client documents

--- settings/clients/mac.go
+++ settings/clients/mac.go
@@ -1,5 +1,38 @@
 package client
 
+// Usage
+// 1. Setup document
+//     SetupDoc(short, long, default) | S(short, long)
+//         CAN BE CHAIN TO [ SetDoc(short, long) ] | [ SetDefault(default) | D(default) ]
+// 2. Prompt (optional)
+//     [ WithPromptYesNo() | YN() | WithPromptYesNoValue(val) | YNV(val) ] -- prompt as yes no question
+//     [ WithPromptValue(value) | V(value) ]                               -- prompt as input question
+//     [ WithPromptChoose(value, list) | C(value, list) ]                  -- prompt as choose from list
+// 2.x Extra information
+//     Prompt will use data from macClient struct, which is
+//       1. Help    - mac.long
+//       2. Title   - mac.short
+//       3. Default - mac.defaultString
+// 3. Bypass (optional)
+//     [ ByPass() | P() ]                -- by default, Application will NOT chain if user input no/false to prompt
+//                                       -- chain by this will suspended this behavior
+// 4. Action
+//     [ DefaultWrite(args) | W(args) ]  -- write setting thought defaults command in macOS
+//     [ DefaultRead(args) | R(args) ]   -- read setting by defaults command in macOS
+//     [ Reset(app) ]                    -- kill application to apply
+// 5. Extra chain to other macClient
+//     On 'DefaultWrite' and 'DefaultRead' commands will return struct call callBack
+//     that exist 1 method called 'Chain(short, long)', This method act like 'SetupDoc()'
+//     so you can chain to other command directly. This chain will STOP if match one of this condition
+//         1. User enter 'no' in YesNoQuestion
+//
+// Example
+// 1. client.S("short help", "long help").
+//      D("false").
+//      YN().
+//      P().
+//      DefaultWrite("some", "command")
+
 import (
 	"fmt"
 
@@ -65,9 +98,22 @@ func (mac macClient) WithPromptYesNo() macClient {
 	return mac
 }
 
+func (mac macClient) WithPromptYesNoValue(value *string) macClient {
+	if mac.ask {
+		util.PromptYesNoWithValue(value, mac.defaultString == "true", mac.short, mac.long)
+		mac.ask = *value == "true"
+	}
+	return mac
+}
+
 // YN is a short command of WithPromptYesNo
 func (mac macClient) YN() macClient {
 	return mac.WithPromptYesNo()
+}
+
+// YN is a short command of WithPromptYesNo
+func (mac macClient) YNV(value *string) macClient {
+	return mac.WithPromptYesNoValue(value)
 }
 
 func (mac macClient) WithPromptValue(value *string) macClient {
@@ -92,6 +138,16 @@ func (mac macClient) WithPromptChoose(value *string, list []string) macClient {
 
 func (mac macClient) C(value *string, list []string) macClient {
 	return mac.WithPromptChoose(value, list)
+}
+
+func (mac macClient) ByPass() macClient {
+	mac.ask = true
+	return mac
+}
+
+// YN is a short command of ByPass
+func (mac macClient) P() macClient {
+	return mac.ByPass()
 }
 
 func (mac macClient) DefaultWrite(args ...string) callBack {
